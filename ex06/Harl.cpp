488e97bbb683663bb0de3300b2ad57f1ea457a7e
Xyloo <adprzyby@student.42heilbronn.de>
2024-10-28 17:58:52 +0100

    Changed a bit communicates and main function to accept arguments

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,26 +6,26 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:22:17 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/10/28 17:25:18 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:58:04 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
 void Harl::debug( void ) {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+	std::cout << "[ DEBUG ]\n" << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
 }
 
 void Harl::info( void ) {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put "
+	std::cout << "[ DEBUG ]\n" << "I cannot believe adding extra bacon costs more money. You didn’t put "
 			 "enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 void Harl::warning( void ) {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for "
+	std::cout << "[ DEBUG ]\n" << "I think I deserve to have some extra bacon for free. I’ve been coming for "
 			 "years whereas you started working here since last month.\n";
 }
 void Harl::error( void ) {
-	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+	std::cout << "[ DEBUG ]\n" << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
 void Harl::complain( std::string level ) {
@@ -37,5 +37,5 @@ void Harl::complain( std::string level ) {
 			return ;
 		}
 	}
-	std::cout << "Invalid complaint\n";
+	std::cout << "[ Probably complaining about insignificant problems ]\n";
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,17 +6,19 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:23:06 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/10/28 17:24:39 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:55:28 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main () {
+int main (int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cout << "Wrong usage! Correct usage: \n" << "./harlFilter *compliant type*\n";
+		return (1);
+	}
 	Harl harl;
-	std::string input;
-	std::cout << "Enter complaint: \n";
-	std::cin >> input;
+	std::string input = argv[1];
 	harl.complain(input);
 	return (0);
 }
