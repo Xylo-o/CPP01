5b5beb5973d83db41756de08b8b95ba65c3c3620
Xyloo <adprzyby@student.42heilbronn.de>
2024-10-31 15:30:31 +0100

    Builded complaint function based on switch function, but there's a segfault to repair

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:22:17 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/10/28 17:58:04 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/10/31 15:28:20 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,53 @@ void Harl::debug( void ) {
 }
 
 void Harl::info( void ) {
-	std::cout << "[ DEBUG ]\n" << "I cannot believe adding extra bacon costs more money. You didn’t put "
+	std::cout << "[ INFO ]\n" << "I cannot believe adding extra bacon costs more money. You didn’t put "
 			 "enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 void Harl::warning( void ) {
-	std::cout << "[ DEBUG ]\n" << "I think I deserve to have some extra bacon for free. I’ve been coming for "
+	std::cout << "[ WARNING ]\n" << "I think I deserve to have some extra bacon for free. I’ve been coming for "
 			 "years whereas you started working here since last month.\n";
 }
 void Harl::error( void ) {
-	std::cout << "[ DEBUG ]\n" << "This is unacceptable! I want to speak to the manager now.\n";
+	std::cout << "[ ERROR ]\n" << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
 void Harl::complain( std::string level ) {
+	int complaint = 0;
 	void (Harl::*functions[])() = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
 	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	for (size_t i = 0; i < 4; ++i) {
 		if (levels[i] == level) {
-			(this->*functions[i])();
-			return ;
+			complaint = i;
 		}
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]\n";
+	switch (complaint) {
+		case 1:
+			for (int i = complaint; i <= 4; i++) {
+				(this->*functions[i])();
+				std::cout << std::endl;
+			}
+			break;
+		case 2:
+			for (int i = complaint; i <= 4; i++) {
+				(this->*functions[i])();
+				std::cout << std::endl;
+			}
+			break;
+		case 3:
+			for (int i = complaint; i <= 4; i++) {
+				(this->*functions[i])();
+				std::cout << std::endl;
+			}			
+			break;
+		case 4:
+			for (int i = complaint; i <= 4; i++) {
+				(this->*functions[i])();
+				std::cout << std::endl;
+			}			
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+			break;
+	}
 }
